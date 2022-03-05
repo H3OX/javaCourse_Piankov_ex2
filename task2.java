27e9b4f161cba5ccaba008358350127d224d1e33
Георгий Пьянков <george@Georgijs-Mac-mini.local>
2022-03-06 01:59:07 +0300

    init

---
+++ task2.java
@@ -0,0 +1,8 @@
+public class task2 {
+    public static void main(String[] args) {
+        String name = "Alex";
+        int b = 3;
+        double c = 1.5;
+        System.out.printf("Имя: %s, Число: %d, Дробное число: %s", name, b, c);
+    }
+}
